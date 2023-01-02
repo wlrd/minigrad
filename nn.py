9bdacae27775889f6c0e9a1eb127bead4e290a08
Waleed Malik <spitfire5295@gmail.com>
2023-01-01 21:04:56 -0500

    add nn framework

---
+++ nn.py
@@ -0,0 +1,42 @@
+import random
+from engine import Value
+
+class Neuron:
+
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = [Value(random.uniform(-1,1))]
+
+    def __call__(self, x):
+        # create activation as wx + b, then apply tanh on it
+        activation = sum(wi*xi for wi, xi in zip(self.w, x)) + self.b
+        return act.tanh()
+    
+    def parameters(self):
+        return self.w + [self.b]
+
+class Layer:
+
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    
+    def __call__(self, x):
+        outs = [neuron(x) for neuron in self.neurons]
+        return outs[0] if len(outs) == 1 else outs
+    
+    def parameters(self):
+        return [p for neuron in self.neurons for p in neuron.parameters]
+
+class MLP:
+
+    def __init__(self, nin, nouts):
+        sz = [nin] + nouts
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+    
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    
+    def parameters(self):
+        return [p for layer in self.layers for p in layers.parameters()]
